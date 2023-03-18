3a067c596f1561c79b387aec9260f6da479fa946
sammun1926 <munorusam10@gmail.com>
2023-03-18 06:32:41 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // code your solution here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+ }
+ saturdayFun();
+
+
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`;
+  }
+  mondayWork('work from home');
+
+  function wrapAdjective(par1="*"){
+    return function (par="a hard worker"){
+        return `You are ${par1}${par}${par1}!`
+    };
+  }
+  wrapAdjective("||") ("passionate in programming");
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+ }
+ saturdayFun();
+
+
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`;
+  }
+  mondayWork('work from home');
+
+  function wrapAdjective(par1="*"){
+    return function (par="a hard worker"){
+        return `You are ${par1}${par}${par1}!`
+    };
+  }
+  wrapAdjective("||") ("passionate in programming");
